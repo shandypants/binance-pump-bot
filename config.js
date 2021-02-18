7de2914b5333686c1c2099f77c8af43ce329237b
shandypants <79169850+shandypants@users.noreply.github.com>
2021-02-18 14:24:26 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Fill your api key here
-  API_KEY: '',
+  API_KEY: 'r9fudXw1HEbEHRG85FIxAr98eFxgflSWzo5xTa7j4S0pgo2Wcg4pJehZoSrcyPWK',
   // Fill your api secret here
-  API_SECRET: '',
+  API_SECRET: 'FZWom75KupFd63WN9hVoNwvlTxpMoVK0Ly1DYEoLtbT2FOtyby0QACfvEInm3PyD',
   HTTP_INTERVAL: 500,
 }
